2aaa62f3de08d6bac1a3b23d12054a9976482a60
Aman <amanthapliyal14@gmail.com>
2021-07-20 21:09:03 +0530

    fixed all bugs

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ let textdata=document.querySelectorAll(".textdata");
 let header=document.querySelector('.header')
 let count=0;
 console.log(icon_container)
+
+
+window.addEventListener('resize', function(event){
+    // do stuff here
+    if (window.screen.width<=908) {
+        icon_container.disabled=true;
+        console.log(window.screen.width)
+    }else{
+        toggle()
+    }
+    
+    
+  });
+
+//   if (window.screen.width<=908) {
+//     icon_container.disabled=true;
+//     console.log(window.screen.width)
+// }else{
+//     toggle()
+// }
+
 function toggle(){
  icon_container.addEventListener('click',()=>{
      if(count%2==0){
@@ -22,5 +43,6 @@ function toggle(){
  });
 }
 
-toggle()
 
+
+toggle()
